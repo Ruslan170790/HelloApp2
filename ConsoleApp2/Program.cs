6c2ecd4be319f6de66b3ab39fd2b1f8666c4d39d
Admin <Admin@DESKTOP-EDJBDJ0>
2020-06-04 00:39:04 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write ("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write ("Введите вораст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите рост: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите размер зарплаты: ");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Имя {name} Возраст {age} Рост {height}м Зарплата {salary}$");
+            Console.ReadKey();
+        }
+    }
+}
